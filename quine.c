450db44e167c002685b41d321b6ab5192d0b431e
Matthias Goergens <matthias.goergens@citrix.com>
2012-05-29 16:07:41 +0100

    Removed superfluous ;

--- quine.c
+++ quine.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* q = "/* This program will print its own source when run.\n */\n#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n\nchar* q = \"%s\";\n\nchar* escape (char* s) {\n  char* r = (char*) malloc ((2*strlen(s)+1)*sizeof(char));;\n  int i = 0;  int j = 0;\n  for (; (r[j] = s[i++]);)\n    switch (r[j]) {\n    case '\\\\': r[j++] = '\\\\'; r[j++] = '\\\\'; break;\n    case '\\n': r[j++] = '\\\\'; r[j++] = 'n'; break;\n    case '\"':  r[j++] = '\\\\'; r[j++] = '\"'; break;\n    default: j++; break;\n    }\n  return r;\n}\n\nint main(int argc, char** args) {\n  printf (q, escape(q));\n  return 0;\n}\n";
+char* q = "/* This program will print its own source when run.\n */\n#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n\nchar* q = \"%s\";\n\nchar* escape (char* s) {\n  char* r = (char*) malloc ((2*strlen(s)+1)*sizeof(char));\n  int i = 0;  int j = 0;\n  for (; (r[j] = s[i++]);)\n    switch (r[j]) {\n    case '\\\\': r[j++] = '\\\\'; r[j++] = '\\\\'; break;\n    case '\\n': r[j++] = '\\\\'; r[j++] = 'n'; break;\n    case '\"':  r[j++] = '\\\\'; r[j++] = '\"'; break;\n    default: j++; break;\n    }\n  return r;\n}\n\nint main(int argc, char** args) {\n  printf (q, escape(q));\n  return 0;\n}\n";
 
 char* escape (char* s) {
-  char* r = (char*) malloc ((2*strlen(s)+1)*sizeof(char));;
+  char* r = (char*) malloc ((2*strlen(s)+1)*sizeof(char));
   int i = 0;  int j = 0;
   for (; (r[j] = s[i++]);)
     switch (r[j]) {
